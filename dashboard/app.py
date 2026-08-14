50fa1ff9223fd0ac19a72233fd71cc55a88792f6
Ojei David <davidojei75@gmail.com>
2026-08-14 23:22:28 +0100

    SHAP EXPLAINABILTY AND FIX, HYDRAULIC FINALLY ADDED, IMS FIXED AND THATS ALL I GUESS

--- dashboard/app.py
+++ dashboard/app.py
@@ -109,7 +109,7 @@ def shap_panel(top_features, explanation=None, title="Why this prediction"):
 from streamlit_option_menu import option_menu
 
 with st.sidebar:
-    st.markdown("## Goeuubel")
+    st.markdown("## Goeujhnkmubel")
     st.caption("Predictive Maintenance Platform")
 
     page = option_menu(
