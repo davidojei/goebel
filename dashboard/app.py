4a2b942c0f3b787a7564e827c3ee108e5f2b9603
Ojei David <davidojei75@gmail.com>
2026-08-17 20:06:39 +0100

    DOCKER FINALLY IN PLACE I THINK

--- dashboard/app.py
+++ dashboard/app.py
@@ -110,7 +110,7 @@ from streamlit_option_menu import option_menu
 
 with st.sidebar:
     st.markdown("## Goebel")
-    st.caption("Predictive Mafffintenance Platform")
+    st.caption("Predictive Maffintenance Platform")
 
     page = option_menu(
         menu_title=None,
